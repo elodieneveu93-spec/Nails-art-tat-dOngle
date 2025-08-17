c181e0b7c5532d03a9bd65c8bcf912384be25944
elodieneveu93-spec <elodie.neveu.93@gmail.com>
2025-08-17 14:26:35 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,4 @@
-{
-  "name": "presson-configurator",
-  "version": "1.0.0",
-  "private": true,
-  "scripts": {
-    "dev": "next dev",
-    "build": "next build",
-    "start": "next start"
-  },
-  "dependencies": {
-    "next": "14.2.3",
-    "react": "18.2.0",
-    "react-dom": "18.2.0"
-  }
-}
+
 export default function Home() {
   // --- mini state manager (sans dépendances)
   const state = {
