d1231d715fe6941207e85a141749dcc881ad7523
elodieneveu93-spec <elodie.neveu.93@gmail.com>
2025-08-17 14:07:41 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      fontFamily: "sans-serif", 
+      display: "flex", 
+      justifyContent: "center", 
+      alignItems: "center", 
+      height: "100vh",
+      background: "#f5f5f5"
+    }}>
+      <h1>🎉 Mon configurateur de Press-On Nails arrive bientôt !</h1>
+    </div>
+  )
+}
